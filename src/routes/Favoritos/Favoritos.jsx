63bb97fa887b7af5600554277b6a6feabb6f7ef0
Matheus Link <linksauermths2203@gmail.com>
2024-01-09 21:23:57 -0300

    Rota dos Favoritos sendo adicionada

---
+++ src/routes/Favoritos/Favoritos.jsx
@@ -0,0 +1,12 @@
+import Voltar from "../../Components/Voltar"
+
+const Favoritos = () => {
+  return (
+    <main>
+        <Voltar rotaDestino="/tutoriais"/>
+        <h1>Favoritos</h1>
+    </main>
+  )
+}
+
+export default Favoritos
